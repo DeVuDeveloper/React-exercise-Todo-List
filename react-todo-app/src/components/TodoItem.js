553087312b7d6c7e1907a485f60f9b22509d56a2
VuDej <drvu47@gmail.com>
2022-02-10 19:02:55 +0100

    set components and start event

---
+++ react-todo-app/src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class TodoItem extends React.Component {
+  render() {
+    return (
+        <li>
+          <input type="checkbox"
+           checked={this.props.todo.completed}
+           onChange={() => console.log("clicked")}
+          />
+          {this.props.todo.title}
+        </li>
+      )
+  }
+}
+
+export default TodoItem
