f1d82e8eb468f290e18c5c34c8c7a9a34486c2dc
VuDej <drvu47@gmail.com>
2022-02-12 09:25:21 +0100

    router and navbar added

---
+++ build/precache-manifest.94c6d7fddb3d276273e911f1472016d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d558518a61309c52f2ef47079780394f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a952e5b37d1127e74c65",
+    "url": "/static/css/main.1e593b3c.chunk.css"
+  },
+  {
+    "revision": "be9a10b2de98ad2b12f7",
+    "url": "/static/js/2.519ed5d6.chunk.js"
+  },
+  {
+    "revision": "a952e5b37d1127e74c65",
+    "url": "/static/js/main.94cc83c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/pages/NotMatch.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+const About = () => {
+  return <div>404 not found</div>
+}
+export default About
