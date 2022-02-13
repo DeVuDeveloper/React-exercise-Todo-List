08ab4da112d447b4ff396abd903da7f2a2e550d2
VuDej <drvu47@gmail.com>
2022-02-13 14:07:12 +0100

    Updates

---
+++ precache-manifest.1631aed92cc3d09b7378cc06434aa03f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297974927d63d868a12d2d94ccba88fb",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "dbb1b9068b46ace4b27e",
+    "url": "/repository-name/static/css/main.81fda377.chunk.css"
+  },
+  {
+    "revision": "972000239b16a1e848c7",
+    "url": "/repository-name/static/js/2.454e20bd.chunk.js"
+  },
+  {
+    "revision": "dbb1b9068b46ace4b27e",
+    "url": "/repository-name/static/js/main.a3ad8ff4.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
--- src/pages/NotMatch.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-
-const About = () => {
-  return <div>404 not found</div>
-}
-export default About
