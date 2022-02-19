7118ff0e08a8a4b6a618f74bf7a13f879cfce36e
VuDej <drvu47@gmail.com>
2022-02-19 12:25:37 +0100

    gh-pages

---
+++ build/precache-manifest.ce9a028071519504eea82754afb242bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac4150cd2df8c64c43def12199d2834",
+    "url": "/React-exercise-Todo-List/index.html"
+  },
+  {
+    "revision": "8445d5ff50c8bc9acb62",
+    "url": "/React-exercise-Todo-List/static/css/main.f7dea68c.chunk.css"
+  },
+  {
+    "revision": "426fd30239e4664c5f02",
+    "url": "/React-exercise-Todo-List/static/js/2.73ade813.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/React-exercise-Todo-List/static/js/2.73ade813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8445d5ff50c8bc9acb62",
+    "url": "/React-exercise-Todo-List/static/js/main.33b3b658.chunk.js"
+  },
+  {
+    "revision": "c49ddaa55ca43128edb3",
+    "url": "/React-exercise-Todo-List/static/js/runtime-main.83d86257.js"
+  }
+]);
